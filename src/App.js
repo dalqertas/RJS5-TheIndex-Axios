f0f9eadfe00dd42dc5ec1fcfcca33e40e6a25bcf
Dunya Al-Qertas <dunya@Dunyas-MacBook-Pro.local>
2020-01-12 21:03:43 +0300

    task done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from 'axios';
 
 // Components
 import Sidebar from "./Sidebar";
@@ -9,10 +8,21 @@ import AuthorDetail from "./AuthorDetail";
 
 class App extends Component {
   state = {
-    currentAuthor: null
+    currentAuthor: null,
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async author => {    try{
+    this.setState({loading:true})
+    let response = await axios.get("https://the-index-api.herokuapp.com/api/authors/"+author.id);
+      this.setState({currentAuthor: response.data, loading:false})
+      
+      } catch(error){
+        console.error(error);
+      }
+
+  }
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
@@ -20,10 +30,20 @@ class App extends Component {
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return <AuthorsList authors={authors} selectAuthor={this.selectAuthor} />;
+      return <AuthorsList authors={this.state.authors} selectAuthor={this.selectAuthor} />;
     }
   };
+  async componentDidMount(){
+    try{
+    let response = await axios.get("https://the-index-api.herokuapp.com/api/authors/");
+      this.setState({authors: response.data, loading:false})
+      
+      } catch(error){
+        console.error(error);
+      }
 
+    }
+  
   render() {
     return (
       <div id="app" className="container-fluid">
@@ -31,7 +51,9 @@ class App extends Component {
           <div className="col-2">
             <Sidebar unselectAuthor={this.unselectAuthor} />
           </div>
-          <div className="content col-10">{this.getContentView()}</div>
+          <div className="content col-10">{this.state.loading === true? 
+          <p> Authors Loading...</p>:
+           this.getContentView()}</div>
         </div>
       </div>
     );
--- src/BookRow.js
+++ src/BookRow.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function BookRow(props) {
   const book = props.book;
-  const authorName = props.authorName;
+  //const authorName = props.authorName;
   return (
     <tr>
       <td>{book.title}</td>
-      <td>{authorName}</td>
+      <td>{book.authors.map(author => author.name+' ')}</td>
       <td>
         <button className="btn" style={{ backgroundColor: book.color }} />
       </td>
